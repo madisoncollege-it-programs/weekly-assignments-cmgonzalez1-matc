52b1bb2c62073c2b5b3b437eb859a1acc8788ca9
cmgonzalez1-matc <cmgonzalez1@madisoncollege.edu>
2022-04-06 12:20:07 -0500

    (Ready) Temp Conversion

---
+++ week9/tempconversion.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#Import the c2f script - Ask the user to input temperature/measure
+import c2f
+print("======" * 9)
+#Import f2c script - Ask the user to input temerature/measure
+import f2c
